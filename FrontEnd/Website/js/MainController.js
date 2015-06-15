28ee181de70898362f708ab0820b1f449948eca5
ravensinth <visser.p.b@googlemail.com>
2015-06-15 13:28:40 +0200

    basic REST GET working

--- FrontEnd/Website/js/MainController.js
+++ FrontEnd/Website/js/MainController.js
@@ -1,16 +1,5 @@
 app.controller('MainController', ['$scope', 'Auction', function($scope, Auction) { 
-  $scope.highestBid = 50;
-  $scope.endDate = '18:05:09';
-  $scope.auction =
-  {
-    "auctionID": 666,
-    "auctionDetails": {
-        "title": "Tollster titel",
-        "description": "description",
-        "imageURL": "example.com",
-        "endDate": 1000
-    }
-  }
+
   $scope.auction = Auction.query();
   // $scope.auction = Auction.get({666}, function(auction) {
 	
